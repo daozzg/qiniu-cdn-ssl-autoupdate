7c4c322a783a5171c050156e13b6edd8f56a4d53
arthur <yahui9119@163.com>
2018-10-10 15:13:08 +0800

    fix shell & python

--- startup.sh
+++ startup.sh
@@ -18,7 +18,7 @@ service cron start
 mkdir -p /ssl/${SSLDOMAIN}/
 touch /var/log/acme.sh.log
 $acme   --issue   --dns dns_dp -d ${SSLDOMAIN} -d *.${SSLDOMAIN}
-$acme  --installcert  -d *.${SSLDOMAIN}    \
+$acme  --installcert  -d ${SSLDOMAIN}    \
         --key-file   /ssl/${SSLDOMAIN}/privkey.pem \
         --fullchain-file /ssl/${SSLDOMAIN}/fullchain.pem \
         --reloadcmd  "python /app/update_cdn_sslcert.py" --debug --log  "/var/log/acme.sh.log"
--- update_cdn_sslcert.py
+++ update_cdn_sslcert.py
@@ -27,10 +27,13 @@ with open(ca, 'r') as f:
 
 ret, info = domain_manager.create_sslcert("{}/{}".format(ssl_domain_name, time.strftime("%Y-%m-%d", time.localtime())),
                                           ssl_domain_name, privatekey_str, ca_str)
-print(ret['certID'])
+print(ret)
+print(info)
 cdn_domain_names_arrs = cdn_domain_names.split(',')
 print(cdn_domain_names_arrs)
 for cdn_domain in cdn_domain_names_arrs:
-    ret, info = domain_manager.put_httpsconf(cdn_domain, ret['certID'], False)
-    print(info)
+    print(cdn_domain)
+    r, i = domain_manager.put_httpsconf(cdn_domain, ret['certID'], False)
+    print(r)
+    print(i)
     pass
